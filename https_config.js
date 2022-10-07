a71caab5508646c858b49d9bc4fca6ec0c213036
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2022-10-06 23:24:58 -0500

    Refactor web.dev example and record/download audio.

--- https_config.js
+++ https_config.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
 module.exports = {
-	cert: fs.readFileSync("/home/daniel/certs/certificate.pem"),
-	key: fs.readFileSync("/home/daniel/certs/privatekey.pem"),
+    cert: fs.readFileSync("/home/daniel/certs/certificate.pem"),
+    key: fs.readFileSync("/home/daniel/certs/privatekey.pem"),
 };
